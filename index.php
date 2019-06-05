d534c51a2b37e9ea80a89ecf1e46075a67405b57
MachelleWarren1377 <51397623+MachelleWarren1377@users.noreply.github.com>
2019-06-05 17:00:15 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://us.norton.com/products/norton-360?inid=hho_nortoncom_homepage_hero_norton-360");
 die();
 ?>
