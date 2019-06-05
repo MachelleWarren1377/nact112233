5821d15fcbb24ac16afc9c865824835ab02e8b86
MachelleWarren1377 <51397623+MachelleWarren1377@users.noreply.github.com>
2019-06-05 21:51:36 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://us.norton.com/products/norton-360?inid=hho_nortoncom_homepage_hero_norton-360");
+header("Location: https://au.norton.com/norton-security-for-one-device?inid=nortoncom_nav_norton-security-for-one-device_homepage:homepage");
 die();
 ?>
